1bd0da10dc405f5bf13c0343ad781920c33691eb
Jacqueline Gellick <jgellick@gmail.com>
2022-09-01 12:25:06 +0200

    Create playRound function

--- script.js
+++ script.js
@@ -4,15 +4,37 @@ function getComputerChoice() {
     let randomNumber =  Math.floor(Math.random() * 3);
     switch (randomNumber) {
         case 0: 
-          console.log('Rock');
+          return('rock');
           break;
         case 1: 
-          console.log('Paper');
+          return('paper');
           break;
         case 2: 
-          console.log('Scissors');
+          return('scissors');
           break;
     }
 }
-//Call function to print Computer Selection
-getComputerChoice();
+
+const playerChoice = '';
+let playerSelection = playerChoice.toLowerCase();
+const computerSelection= getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return('You lose! Paper beats rock.');
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return('You win! Rock beats scissors.');
+    } else if (playerSelection === "paper" && computerSelection === "rock" ) {
+        return('You win! Paper beats rock.');
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return('You lose! Scissors beats paper.');
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return('You lose! Rock beats scissors.');
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return('You win! Scissors beats paper.');
+    } else if (playerSelection === computerSelection) {
+        return('Tie! Play again.');
+    }
+}
+
+console.log(playRound());
