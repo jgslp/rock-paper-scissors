de55903c8765f29f2d6089697b9e4d8829cb062b
Jacqueline Gellick <jgellick@gmail.com>
2022-09-09 13:05:11 +0200

    Add title and header

--- script.js
+++ script.js
@@ -15,26 +15,29 @@ function getComputerChoice() {
     }
 }
 
-const playerChoice = '';
+//Convert player input to lowercase
+let playerChoice = 'rock';
 let playerSelection = playerChoice.toLowerCase();
 const computerSelection= getComputerChoice();
 
+//Compare selections
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "paper") {
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
         return('You lose! Paper beats rock.');
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         return('You win! Rock beats scissors.');
-    } else if (playerSelection === "paper" && computerSelection === "rock" ) {
+    } else if (playerSelection === 'paper' && computerSelection === 'rock' ) {
         return('You win! Paper beats rock.');
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         return('You lose! Scissors beats paper.');
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         return('You lose! Rock beats scissors.');
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         return('You win! Scissors beats paper.');
     } else if (playerSelection === computerSelection) {
         return('Tie! Play again.');
     }
 }
 
-console.log(playRound());
+//Print result of round
+console.log(playRound(playerSelection, computerSelection));
