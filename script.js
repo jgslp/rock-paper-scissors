d0b472c5cd1d546c59b7c9b11e9a136b7ac5c25c
Jacqueline Gellick <jgellick@gmail.com>
2022-11-13 19:30:16 +0100

    Redid playround function and five rounds loop

--- script.js
+++ script.js
@@ -1,67 +1,49 @@
-
-// //Determine computer selection
-// function getComputerChoice() {
-//     let randomNumber =  Math.floor(Math.random() * 3);
-//     switch (randomNumber) {
-//         case 0: 
-//           return('rock');
-//           break;
-//         case 1: 
-//           return('paper');
-//           break;
-//         case 2: 
-//           return('scissors');
-//           break;
-//     }
-// }
-// let playerScore = 0;
-// let computerScore = 0;
-// let gamesPlayed = 0;
-
-// //Prompt user and convert player input to lowercase
-// let playerChoice = prompt('Type Rock, Paper, or Scissors');
-// let playerSelection = playerChoice.toLowerCase();
-// const computerSelection= getComputerChoice();
-
-// //Compare player to computer
-// function playRound(playerSelection, computerSelection) {
-//     if (playerSelection === 'rock' && computerSelection === 'paper') {
-//         return('You lose! Paper beats rock.')
-//         computerScore ++;
-//     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-//         return('You win! Rock beats scissors.');
-//         playerScore++;
-//     } else if (playerSelection === 'paper' && computerSelection === 'rock' ) {
-//         return('You win! Paper beats rock.');
-//         playerScore++;
-//     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-//         return('You lose! Scissors beats paper.');
-//         computerScore ++;
-//     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-//         return('You lose! Rock beats scissors.');
-//         computerScore ++;
-//     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-//         return('You win! Scissors beats paper.');
-//         playerScore++;
-//     } else if (playerSelection === computerSelection) {
-//         return('Tie! Play again.');
-//     }
-// }
-
-// //Print round result
-// console.log(playRound(playerSelection, computerSelection));
-
-// //Play five rounds
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         playRound(playerSelection, computerSelection);   
-//     }
-// }
-
 // Determine computer selection
-function computerChoice() {
-    let choice = ["rock", "paper", "scissors"];
-    return choice[Math.floor(Math.random() * 3)];
+function getComputerChoice() {
+    let computerChoice = ["rock", "paper", "scissors"];
+    return computerChoice[Math.floor(Math.random() * 3)];
 }
 
-console.log(computerChoice());
+let playerScore = 0;
+let computerScore = 0;
+
+// Compare computer to player
+function playRound() {
+    const computerSelection= getComputerChoice();
+    playerChoice = prompt('Type Rock, Paper, or Scissors');
+    let playerSelection = playerChoice.toLowerCase();
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore ++;
+        return('You lose! Paper beats rock.');
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++;
+        return('You win! Rock beats scissors.');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock' ) {
+        playerScore++;
+        return('You win! Paper beats rock.');
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore ++;
+        return('You lose! Scissors beats paper.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore ++;
+        return('You lose! Rock beats scissors.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++;
+        return('You win! Scissors beats paper.');
+    } else if (playerSelection === computerSelection) {
+        return('Tie! Play again.');
+    }
+}
+
+console.log(playRound());
+
+
+// Play 5 rounds
+let i = 1;
+while (i < 5) {
+    console.log(playRound());
+    i++;
+}
+
+console.log(playerScore);
+console.log(computerScore);
